ddef796c263133af62ddad981fb421259c52e79b
jason gors <jason.gors.work@gmail.com>
2015-11-06 02:14:49 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-# I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
-# To execute just do this in the `src` dir:
+# To execute just run this script directly with:
+# sh ./run.sh
+# 
+# or do this in the `src` dir:
 # $ python tweet_processor.py
-# or the script can take one argument (the path to a file to use as a data set.
+#
+# Also, the script can take one argument -- the path to a file to use as a data set).
 # $ python tweet_processor.py ../data-gen/tweets.txt
 
 python ./src/tweet_processor.py
