c5cb16ea906b188816ed24b7ead61d201d58bad9
jason gors <jason.gors.work@gmail.com>
2015-11-06 02:23:01 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 # To execute just run this script directly with:
 # sh ./run.sh
 # 
-# or do this in the `src` dir:
-# $ python tweet_processor.py
+# or do this:
+# $ python ./src/tweet_processor.py
 #
 # Also, the script can take one argument -- the path to a file to use as a data set).
-# $ python tweet_processor.py ../data-gen/tweets.txt
+# $ python ./src/tweet_processor.py ../data-gen/tweets.txt
 
 python ./src/tweet_processor.py
