72bd0165c0c8a97b708a33c6725a4f6c8057a3b8
Jason Gors <jason.gors.work@gmail.com>
2015-11-06 11:48:04 -0500

    grammer correction in comment

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# To execute just run this script directly with:
+# To execute, just run this script directly with:
 # sh ./run.sh
 # 
-# or do this:
+# Or this:
 # $ python ./src/tweet_processor.py
 #
-# Also, the script can take one argument -- the path to a file to use as a data set).
+# Also, the script can take one argument -- the path to a data set file.
 # $ python ./src/tweet_processor.py ./data-gen/tweets.txt
 
 python ./src/tweet_processor.py
