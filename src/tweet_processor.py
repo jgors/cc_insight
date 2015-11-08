0bff47a43de1ef08fad41a95d9c12af167ea5a28
Jason Gors <jason.gors.work@gmail.com>
2015-11-08 00:21:43 -0500

    added better inline docs

--- src/tweet_processor.py
+++ src/tweet_processor.py
@@ -3,8 +3,8 @@
 #----------------------------------------------------------------
 # Author: Jason Gors <jasonDOTgorsATgmail>
 # Creation Date: 10-31-2015
-# Purpose:  Creates an interface for incoming tweets via the `Tweet` class,  
-#           which standardizes the tweet's text and generates a timestamp 
+# Purpose:  Creates an interface for incoming tweets via the `Tweet` class,
+#           which standardizes the tweet's text and generates a timestamp
 #           so that the tweet can then be passed into a `TweetsGraph`
 #           instance (which is based on a rolling time window).
 #----------------------------------------------------------------
@@ -12,7 +12,7 @@ import os
 from os import path
 import sys
 import logging
-from json import loads as json_loads 
+from json import loads as json_loads
 from string import translate
 from string import maketrans
 from codecs import encode as codecs_encode
@@ -23,31 +23,29 @@ from tweets_graph import TweetsGraph
 
 
 # FAQ says, "all whitespace escape characters should be replaced with a single space"
-#   And the C language defines whitespace characters to be: 
-#   """...space (" "), horizontal tab ("\t"), new-line ("\n"), 
+#   And the C language defines whitespace characters to be:
+#   """...space (" "), horizontal tab ("\t"), new-line ("\n"),
 #      vertical tab ("\v"), and form-feed ("\f")  -- from [1] and [2]. so i'll use those
 # [1] https://en.wikipedia.org/wiki/Whitespace_character#Programming_languages
-chars_to_replace_with_space = {"\t", "\n", "\r", "\v", "\f"}  #also there is str.isspace()
+# chars_to_replace_with_space = {"\t", "\n", "\r", "\v", "\f"}  #also there is str.isspace()
 white_space_chars = "\t\n\r\v\f"
 whitespace_replacements = ' ' * len(white_space_chars)
-# ws_replaced_txt = ''.join([' ' if char in chars_to_replace_with_space 
-                                # else char for char in ascii_txt])
 trans_table = maketrans(white_space_chars, whitespace_replacements)
 
-# Also, FAQ says only keep ascii chars 32-127 
-# set_of_ascii_we_want_to_keep = set([chr(char) for char in xrange(32,127)])  
+# Also, FAQ says only keep ascii chars 32-127
+# set_of_ascii_we_want_to_keep = set([chr(char) for char in xrange(32,127)])
 
 unicode_tweets_count = 0
 
 
 def clean_text(text_to_convert, count_unicode=False):
     '''Removes unicode and escape chars from text_to_convert;
-    also counts the number of tweets which contained unicode. 
+    also counts the number of tweets which contained unicode.
 
     Parameters
     ----------
-    text_to_convert: (str) to standardize 
-    count_unicode:  either (True) for tweet text or (False) for 
+    text_to_convert: (str) to standardize
+    count_unicode:  either (True) for tweet text or (False) for
         a tweet hashtag
 
 
@@ -55,11 +53,11 @@ def clean_text(text_to_convert, count_unicode=False):
     -------
     an ascii (str) without unicode and escape chars
 
-    
-    Refer to here for more details on tweet formating: 
+
+    Refer to here for more details on tweet formating:
     https://dev.twitter.com/streaming/overview/processing
     '''
-            
+
     try:
         # https://docs.python.org/2/howto/unicode.html
         ascii_txt = codecs_encode(text_to_convert, 'ascii')
@@ -72,25 +70,27 @@ def clean_text(text_to_convert, count_unicode=False):
         ascii_txt = codecs_encode(text_to_convert, 'ascii', 'ignore')
 
     ### replace the whitespace chars with a single space
+    # ws_replaced_txt = ''.join([' ' if char in chars_to_replace_with_space
+    #                                else char for char in ascii_txt])
     ws_replaced_txt = translate(ascii_txt, trans_table)
 
-    return ws_replaced_txt 
+    return ws_replaced_txt
 
 
 
 class Tweet(object):
-    '''Holds relevant information from a tweet so that class instances 
+    '''Holds relevant information from a tweet so that class instances
     can be passed into a TweetsGraph instance (to build a hashtag graph).
 
 
     Attributes
     ----------
-    timestamp: takes tweet `created_at` (str) field and converts to an 
-        epoch based time format; used to better perform time based 
+    timestamp: takes tweet `created_at` (str) field and converts to an
+        epoch based time format; used to better perform time based
         comparisons and manipulations.
 
-    hashtags: takes a (list) of a tweet's hashtags and returns them with 
-        unicode and escape chars removed.  Will either return an empty 
+    hashtags: takes a (list) of a tweet's hashtags and returns them with
+        unicode and escape chars removed.  Will either return an empty
         set or a set of length >= 2'''
 
 
@@ -102,8 +102,8 @@ class Tweet(object):
         hashtags:   (list) of hashtags (strs) obtained from the Twitter api json output
         '''
 
-        self.timestamp = timegm(strptime(created_at, "%a %b %d %H:%M:%S +0000 %Y")) 
-        
+        self.timestamp = timegm(strptime(created_at, "%a %b %d %H:%M:%S +0000 %Y"))
+
         hashtags_cleaned = [clean_text(hashtag).lower() for hashtag in hashtags]
         hashtags_to_use = set([ht for ht in hashtags_cleaned if hashtag])
         hashtags_for_graph = hashtags_to_use if len(hashtags_to_use) >= 2 else set()
@@ -119,8 +119,8 @@ if __name__ == '__main__':
         assert path.isfile(tweets_data_incoming), "Error: need to pass in a data file that exists."
         tweets_incomming_path = path.abspath(tweets_data_incoming)
     else:
-        # to run the actual test data set (needs to be run from root of repo!) 
-        tweets_incomming_path = path.abspath(path.join('tweet_input', 'tweets.txt'))     
+        # to run the actual test data set (needs to be run from root of repo!)
+        tweets_incomming_path = path.abspath(path.join('tweet_input', 'tweets.txt'))
     tweet_output_path = path.abspath('tweet_output')
 
     if not path.isdir(tweet_output_path):
@@ -132,7 +132,7 @@ if __name__ == '__main__':
 
     tweet_graph = TweetsGraph(time_window=60)
 
-    with open(tweets_incomming_path, 'r') as tweets_incomming:  
+    with open(tweets_incomming_path, 'r') as tweets_incomming:
         # all tweets from the api are utf-8 encoded:
         # https://dev.twitter.com/overview/api/counting-characters
         for cnt, tweet in enumerate(tweets_incomming, start=1):
@@ -141,7 +141,7 @@ if __name__ == '__main__':
                 text = tweet_dict["text"]
                 created_at = tweet_dict["created_at"]
                 hashtags = [hashtag['text'] for hashtag in tweet_dict['entities']['hashtags']]
-                tweet = Tweet(created_at, hashtags) 
+                tweet = Tweet(created_at, hashtags)
                 tweet_graph.update_graph(tweet)
 
                 cleaned_text = clean_text(text, count_unicode=True)
@@ -152,7 +152,7 @@ if __name__ == '__main__':
                 ft2.write('{}\n'.format(tweet_graph.get_graph_avg_degree_of_all_nodes()))
 
             except Exception as e:  #  don't normally exception handle in main like this, but play it safe on unknown data.
-                # logging.exception("Tweet on ln {} failed work.  Exception {}".format(cnt, e))
+                # logging.exception("Tweet on ln {} failed to work.  Exception {}".format(cnt, e))
                 pass
 
         ft1.write('\n{} tweets contained unicode.'.format(unicode_tweets_count))
--- src/tweets_graph.py
+++ src/tweets_graph.py
@@ -12,9 +12,9 @@ from itertools import permutations
 
 
 class TweetsGraph(object):
-    """Initalized with a `time_window` argument (default: 60) and returns 
-    an empty graph data-structure.  The graph can then be built and maintained 
-    by succesively passing `Tweet` instances into the `update_graph` method. 
+    """Initalized with a `time_window` argument (default: 60) and returns
+    an empty graph data-structure.  The graph can then be built and maintained
+    by succesively passing `Tweet` instances into the `update_graph` method.
         eg. 
             tweet_graph = TweetsGraph()
             tweet = Tweet(created_at, hashtags)
@@ -22,19 +22,19 @@ class TweetsGraph(object):
 
         (see `Tweet` class in `tweet_processing.py` for it's details)
 
-        
+
     Methods
     -------
-    update_graph: updates a graph instance by adding all permutations of a 
+    update_graph: updates a graph instance by adding all permutations of a
         tweet's hashtag pairs as nodes and adjacent neighboring nodes.
 
 
     Attributes
     ----------
-    graph: (dict) where each key is a node/hashtag and each corresponding value  
+    graph: (dict) where each key is a node/hashtag and each corresponding value
         is a set of it's adjacent neighboring nodes/hashtags (eg. adjacency set)
 
-    time_window: (int) that specifies how long back (in seconds) old tweets should 
+    time_window: (int) that specifies how long back (in seconds) old tweets should
         remain in the graph before being removed (Default: 60)
 
 
@@ -45,10 +45,10 @@ class TweetsGraph(object):
 
     Notes
     -----
-    Each succesive tweet submitted into a graph instance (via the `update_graph` 
-    method) adds the new tweet's hashtags into the graph (if there are any) 
-    and removes all hashtag node neighbors (and any nodes themselves if they 
-    are empty) that are older than the specified `time_window` when compared to 
+    Each succesive tweet submitted into a graph instance (via the `update_graph`
+    method) adds the new tweet's hashtags into the graph (if there are any)
+    and removes all hashtag node neighbors (and any nodes themselves if they
+    are empty) that are older than the specified `time_window` when compared to
     the latest tweet."""
 
 
@@ -60,13 +60,13 @@ class TweetsGraph(object):
 
 
     def __update_active_hashtag_nodes(self, current_timestamp, current_hashtags):
-        '''This does all of the work in maintaining that only tweets that are  
+        '''This does all of the work in maintaining that only tweets that are
         within the `time_window` prior to the current tweet are used in making up
-        the hashtag graph.  This uses a first in, first out (fifo) ordering for 
-        maintaining that the newest timestamped hashtags are located at the end 
-        of the list and the oldest hashtag timestamps at the beginning of the list. 
+        the hashtag graph.  This uses a first in, first out (fifo) ordering for
+        maintaining that the newest timestamped hashtags are located at the end
+        of the list and the oldest hashtag timestamps at the beginning of the list.
 
-        
+
         Parameters
         ----------
         current_timestamp:  epoch based time (int)
@@ -75,20 +75,20 @@ class TweetsGraph(object):
 
         Notes
         -----
-        When each new tweet is passed in, a test checks whether it is timestamped  
-        as more than the `time_window` amount allowed after the first/oldest tweet 
-        was passed into the list; if so, then the test checks whether the same holds 
-        true for the second tweet in the list, and so on, until the test comes to a 
-        tweet that is within the allowed `time_window` (or reaches the end of he list), 
-        whereupon all tweets that are more than the allowed `time_window` away from 
-        this latest tweet are removed; finally, the newest tweet gets appended onto 
+        When each new tweet is passed in, a test checks whether it is timestamped
+        as more than the `time_window` amount allowed after the first/oldest tweet
+        currently at the beg of the list; if so, then the test checks whether the same
+        holds true for the second tweet in the list, and so on, until the test comes to
+        a tweet that is within the allowed `time_window` (or reaches the end of he list),
+        whereupon all tweets that are more than the allowed `time_window` away from
+        this latest tweet are removed; finally, the newest tweet gets appended onto
         the end of the list so as to serve as the next most recent tweet for next time
         around.'''
 
         # To track what should & shouldn't be included in the graph, this uses 
         # - a list of (unique) timestamps (fifo), along with
-        # - a dict with a timestamp (for each sec) as the keys and a list 
-        # of corresponding hashtag sets as the values; like: 
+        # - a dict with a timestamp (for each sec) as the keys and a list
+        # of corresponding hashtag sets as the values; like:
         #
         # unique_timestamp_list = [timeOldest ... timeNewest]
         # dict_of_timestamps_and_lists_of_hashtag_sets = {timestamp_per_sec: [{hashtag_sets}]}
@@ -110,20 +110,27 @@ class TweetsGraph(object):
                                 self.__remove_hashtags_from_graph(set_of_old_hashtags)
                             if not self.__dict_of_timestamps_and_list_of_hashtag_sets[old_timestamp]: # if only an empty set remains
                                 del self.__dict_of_timestamps_and_list_of_hashtag_sets[old_timestamp]
+                        else:   # only hit this when unsorted data was used in testing.
+                            pass
                     else:
-                        if cnt > 0: # don't recopy if we don't need to (eg. when cnt is 0)     
-                            self.__unique_timestamp_list = self.__unique_timestamp_list[cnt:] 
+                        if cnt > 0: # don't recopy if we don't need to (eg. when cnt is 0)
+                            # after every sec increment this happens, but there are many tweets w/in each sec tick; (api data ~50 a sec)
+                            # -- so this happens after every avg_N_of_tweets_per_second, to a list of only constant size time_window.
+                            # now even on their firehouse data this copying of unique_timestamp_list would still be of only size `time_window`,
+                            # and would happen even more infrequenqently b/c there would be a way larger avg_N_of_tweets_per_second
+                            # being delivered between performing these slicing events.
+                            self.__unique_timestamp_list = self.__unique_timestamp_list[cnt:]
                         break
                 self.__unique_timestamp_list.append(current_timestamp)
-            else:  # next_most_recent_timestamp == current_timestamp b/c tweets will 
-                # be in sorted order, so don't need to append current_timestamp to 
-                # __unique_timestamp_list b/c the current_timestamp isn't unique then 
+            else:  # next_most_recent_timestamp == current_timestamp b/c tweets will
+                # be in sorted order, so don't need to append current_timestamp to
+                # __unique_timestamp_list b/c the current_timestamp isn't unique then
                 pass
         self.__manage_timestamps_and_hashtag_dict(current_hashtags, current_timestamp)
 
 
     def __manage_timestamps_and_hashtag_dict(self, current_hashtags, current_timestamp):
-        '''Determines what the state of the node/neighbor pairings are relative 
+        '''Determines what the state of the node/neighbor pairings are relative
         to when their timestamp occurred
 
 
@@ -137,16 +144,16 @@ class TweetsGraph(object):
             if current_hashtags:
                 self.__dict_of_timestamps_and_list_of_hashtag_sets[current_timestamp].append(current_hashtags)
             else:
-                pass    # b/c we don't want to overwrite whatever might already be there
+                pass    # nothing to do here; no hashtags to add into the graph for this time_stamp
         else:
             if current_hashtags:
                 self.__dict_of_timestamps_and_list_of_hashtag_sets[current_timestamp] = [current_hashtags]
             else:
-                self.__dict_of_timestamps_and_list_of_hashtag_sets[current_timestamp] = [] 
+                self.__dict_of_timestamps_and_list_of_hashtag_sets[current_timestamp] = []
 
 
     def __remove_hashtags_from_graph(self, old_hashtags):
-        '''If an old hashtag node and neighbor pair is in the graph, then this removes it.
+        '''If an old hashtag node and neighbor pair is in the graph, then this removes them.
 
 
         Parameters
@@ -156,18 +163,18 @@ class TweetsGraph(object):
 
         for ht1, ht2 in permutations(old_hashtags, 2):
             if ht1 in self.graph:
-                # need this check b/c since using sets, no duplicates of a hashtag neighbor  
-                # is stored under a hashtag_node; thus a given neighbor might have already of 
+                # need this check b/c since using sets, no duplicates of a hashtag neighbor
+                # is stored under a hashtag_node; thus a given neighbor might have already of
                 # been removed in a previous removal step
-                if ht2 in self.graph[ht1]:     
+                if ht2 in self.graph[ht1]:
                     self.graph[ht1].remove(ht2)   # remove the hashtag_node from the neighbors set
                 # if the hashtag_node doesn't have any more neighbors left, remove it from the graph.
-                if not self.graph[ht1]:     
+                if not self.graph[ht1]:
                     del self.graph[ht1]
-            
+
 
     def update_graph(self, tweet):
-        '''Delegation of building up and/or removing nodes from the graph 
+        '''Delegation of building up and/or removing nodes from the graph
         as they are passed into this method.
 
 
@@ -178,7 +185,7 @@ class TweetsGraph(object):
         if tweet.hashtags:
             # add all new pairs of hashtags into the graph
             for ht1, ht2 in permutations(tweet.hashtags, 2):
-                if ht1 not in self.graph: 
+                if ht1 not in self.graph:
                     self.graph[ht1] = {ht2}
                 else:
                     self.graph[ht1].update({ht2})
@@ -187,11 +194,11 @@ class TweetsGraph(object):
 
 
     def get_graph_avg_degree_of_all_nodes(self):
-        '''Calculates and returns the average degree for all nodes in all 
+        '''Calculates and returns the average degree for all nodes in all
         graphs and subgraphs'''
 
         if self.graph:
-            total_edge_len = sum([len(neighbors) for neighbors in self.graph.itervalues()]) 
+            total_edge_len = sum([len(neighbors) for neighbors in self.graph.itervalues()])
             avg_deg = total_edge_len / float(len(self.graph))
             return "{:.2f}".format(round(avg_deg, 2))
         else:
